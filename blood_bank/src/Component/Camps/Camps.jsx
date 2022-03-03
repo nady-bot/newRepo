38f1948248b2d84c8fee20c940847ff7798ac6b1
Your Name <you@example.com>
2022-03-03 12:04:04 +0200

    nady

--- blood_bank/src/Component/Camps/Camps.jsx
+++ blood_bank/src/Component/Camps/Camps.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Camps() {
   return (
-    <div>Camps</div>
+    <h1 className='text-danger fw-bolder text-center '> Hello React </h1>
   )
 }
--- blood_bank/src/Component/Home/Home.jsx
+++ blood_bank/src/Component/Home/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div >Home from desktop </div>
+    <div > From Hassan </div>
   )
 }
