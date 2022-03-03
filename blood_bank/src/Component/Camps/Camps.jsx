c9866c4d61d917cba759736456fcac8033059f05
Nady <74647837+nady-bot@users.noreply.github.com>
2022-03-03 12:13:11 +0200

    Update Camps.jsx

--- blood_bank/src/Component/Camps/Camps.jsx
+++ blood_bank/src/Component/Camps/Camps.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Camps() {
   return (
-    <h1 className='text-danger fw-bolder text-center '> Hello GitHub </h1>
+    <h1 className='text-info fw-bolder text-center '> Hello GitHub </h1>
   )
 }
