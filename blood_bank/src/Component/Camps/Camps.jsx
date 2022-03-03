763be631f55ecf4f4aeb60d2d24a24afd520e154
Your Name <you@example.com>
2022-03-03 11:35:20 +0200

    Hassan

--- blood_bank/src/Component/Camps/Camps.jsx
+++ blood_bank/src/Component/Camps/Camps.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Camps() {
   return (
-    <div>Camps</div>
+    <div>
+      <h1 className='text-danger fw-bolder text-center '> Hello Wolrd </h1>
+    </div>
   )
 }
