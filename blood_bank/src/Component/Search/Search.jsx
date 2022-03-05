9d9a0338b89eedcb5b9f26a351f3dcb1d4de46be
Nady <74647837+nady-bot@users.noreply.github.com>
2022-03-05 16:07:49 +0200

    Update Search.jsx

--- blood_bank/src/Component/Search/Search.jsx
+++ blood_bank/src/Component/Search/Search.jsx
@@ -1,59 +1,59 @@
 import React from 'react'
-import searchItem from "../Search/search.png"
+import searchItem from "../../images/search.png"
 import style from "../Search/Search.module.css"
 
 export default function Search() {
-  return ( <>
+  return (<>
 
 
 
 
-   <div className={`"container" ${ style.container}`}>
-     <div className="row w-50 mx-auto my-auto shadow bg-light">
-
-     
-
-  <div className="col-xl-12 text-center border border-1 border-danger rounded ">
-
-<img src={searchItem} className=" m-3" alt= "search index "/>
-<div>
-  <div className='row'>
-<div className='col-xl-6'>
-<label htmlFor="bl-group" className='fw-bold'>Choose a blood group </label>
-</div>
-<div className='col-xl-6'>
-<select   name="bl-group" id="bl-group" className='rounded-right'>
-<option value="zero" > Select  </option>
-  <option value="one"> A group </option>
-  <option value="two"> B group</option>
-  <option value="three"> AB group</option>
-  <option value="four"> O group </option>
-</select>
-
-</div>
-
-</div>
+    <div className={`"container" ${style.container}`}>
+      <div className="row w-50 mx-auto my-auto border-sherd box-sherd  bg-sherd">
 
 
-<button type='' className='btn btn-danger px-4 rounded-top rounded-bottom fw-bold m-5'> Search</button>
 
-  </div>
-  </div>
+        <div className="col-xl-12 text-center   rounded ">
+
+          <img src={searchItem} className=" m-3" alt="search index " />
+          <div>
+            <div className='row'>
+              <div className='col-xl-6'>
+                <label htmlFor="bl-group" className='fw-bold'>Choose a blood group </label>
+              </div>
+              <div className='col-xl-6'>
+                <select name="bl-group" id="bl-group" className='rounded-right'>
+                  <option value="zero" > Select  </option>
+                  <option value="one"> A group </option>
+                  <option value="two"> B group</option>
+                  <option value="three"> AB group</option>
+                  <option value="four"> O group </option>
+                </select>
+
+              </div>
+
+            </div>
 
 
+            <button type='' className='btn btn-danger px-4 rounded-top rounded-bottom fw-bold m-5'> Search</button>
+
+          </div>
+        </div>
 
 
 
 
-     </div>
-
-</div>
 
 
+      </div>
+
+    </div>
 
 
-  
-    
-    </>
+
+
+
+
+  </>
   )
 }
