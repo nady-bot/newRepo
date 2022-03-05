ccc81ccfd1071d64531ff5287d5676e460d9d64a
Nady <74647837+nady-bot@users.noreply.github.com>
2022-03-05 16:10:04 +0200

    Update SendRequest.jsx

--- blood_bank/src/Component/SendRequest/SendRequest.jsx
+++ blood_bank/src/Component/SendRequest/SendRequest.jsx
@@ -3,46 +3,50 @@ import img from "../../images/brequest.png"
 import style from "./SendRequest.module.css"
 
 export default function SendRequest() {
+
+
   return (
-    <div className={ style.sendRequest}>
-      <img src = {img} alt = "" /> 
-      <table className=  {style.table }>
-           <tr> 
-             <td> Name : </td>
-             <td>  < input name = "name" />  </td>
-           </tr>
+  
+    <div className={`my-5 bg-sherd border-sherd box-sherd ${style.sendRequest}`}>
+      <img src={img} alt="" />
+      <table className={style.table}>
+        <tbody>
+          <tr>
+            <td> Name : </td>
+            <td>  < input name="name" />  </td>
+          </tr>
 
-           <tr> 
-             <td> Gender : </td>
-             <td> 
-                <input name = "gender" type = "radio" /> 
-                <label> Male </label>
-                 </td><td>
-                <input name = "gender" type = "radio" /> 
-                <label> Female </label>
-                 </td>
-           </tr>
-           <tr> 
-             <td> Age : </td>
-             <td>  <input name = "phoneNo" type = "number" />  </td>
-           </tr>
+          <tr>
+            <td> Gender : </td>
+            <td>
+              <input name="gender" type="radio" />
+              <label> Male </label>
+            
+              <input name="gender" type="radio" />
+              <label> Female </label>
+            </td>
+          </tr>
+          <tr>
+            <td> Age : </td>
+            <td>  <input name="phoneNo" type="number" />  </td>
+          </tr>
 
-           <tr> 
-             <td> Mobile No : </td>
-             <td>  <input />  </td>
-           </tr>
-           <tr> 
-             <td> Select Blood Group : </td>
-             <td>  
-               
-               <select name = "selectOption">
-                 <option> select </option>
-               </select>
+          <tr>
+            <td> Mobile No : </td>
+            <td>  <input />  </td>
+          </tr>
+          <tr>
+            <td> Select Blood Group : </td>
+            <td>
 
-                 </td>
-           </tr>
+              <select name="selectOption">
+                <option> select </option>
+              </select>
 
+            </td>
+          </tr>
 
+        </tbody>
       </table>
 
 
