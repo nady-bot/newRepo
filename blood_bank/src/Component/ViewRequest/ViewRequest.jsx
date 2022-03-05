dd9d400d2a52e01b0b0ad90c0e03d6ee158924dd
Jackleen-Rasmy <86874525+Jackleen-Rasmy@users.noreply.github.com>
2022-03-06 00:35:49 +0200

    Update ViewRequest.jsx

--- blood_bank/src/Component/ViewRequest/ViewRequest.jsx
+++ blood_bank/src/Component/ViewRequest/ViewRequest.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from 'react';
+import imgReq from '../../images/brequest.png';
+import style from './ViewRequest.module.css'
+
 
 export default function ViewRequest() {
   return (
-    <div>ViewRequest</div>
+   <div className="container">
+     <div className="row my-5">
+       <div className={`${style.all} pb-4`}>
+         
+       <div className="col-md-12">
+         <div className={`viewreq    ${style.mydiv} d-flex justify-content-center `  }>
+           <div className="myImg w-25  ">
+           <img  src={imgReq} className='w-100 ' />
+             </div>
+
+             </div>
+
+<div className="col-md-12 my-5">
+
+  <div className="items ">
+  <ul className={`list-unstyled d-flex  justify-content-around  ${style.mydiv}`}>  
+<li>Blood Group</li>
+           <li>Name</li>
+           <li>Gender</li>
+           <li>Contact No</li>
+           <li>Mobile No</li>
+           <li>Email</li>
+<li>Till Required Date</li>
+
+</ul>
+    </div>
+
+           
+
+
+
+  </div>
+
+          
+
+           </div>
+           </div>
+         </div>
+
+       </div>
+   
   )
 }
