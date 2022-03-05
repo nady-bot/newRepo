c879f4663a6468a619d66bd83edb8c95df60bb8b
Jackleen-Rasmy <86874525+Jackleen-Rasmy@users.noreply.github.com>
2022-03-06 00:39:55 +0200

    Update Login.jsx

--- blood_bank/src/Component/Login/Login.jsx
+++ blood_bank/src/Component/Login/Login.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from 'react';
+import logInTwo from '../../images/login2.png'
+import logInOne from '../../images/login1.png'
 
 export default function Login() {
   return (
-    <div>Login</div>
+
+    <div className="container">
+      <div className="row  d-flex justify-content-center">
+        <div className="col-md-4">
+          <div className="alldivs">
+            <div className="img w-100">
+              <img src={logInTwo} className='w-100' />
+              </div>
+
+              <div className="Img-form d-flex justify-content-center">
+                <div className="img2">
+                  <img src={logInOne} className='w-100'/>
+                  </div>
+
+                  <form>
+
+                    <div className="inputs">
+                    <label className='' htmlFor='email'>Email:</label>
+                    <input type='email' name='email'  className=''/>
+                      </div>
+
+                 <div className="inputs">
+                    <label className='' htmlFor='password'>Password:</label>
+                    <input type='password' name='password' className='' />
+                     </div>
+                   
+
+                    <button className='btn btn-danger mt-2'>login</button>
+
+                  </form>
+
+
+
+
+
+                </div>
+            
+            
+            
+            </div>
+          </div>
+        </div>
+        </div>
   )
 }
