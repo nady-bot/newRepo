7e5cf833bfff40bd52c3bd24459b473aa666b838
Nady <74647837+nady-bot@users.noreply.github.com>
2022-03-06 18:37:27 +0200

    Update ViewRequest.jsx

--- blood_bank/src/Component/ViewRequest/ViewRequest.jsx
+++ blood_bank/src/Component/ViewRequest/ViewRequest.jsx
@@ -5,46 +5,46 @@ import style from './ViewRequest.module.css'
 
 export default function ViewRequest() {
   return (
-   <div className="container">
-     <div className="row my-5">
-       <div className={`${style.all} pb-4`}>
-         
-       <div className="col-md-12">
-         <div className={`viewreq    ${style.mydiv} d-flex justify-content-center `  }>
-           <div className="myImg w-25  ">
-           <img  src={imgReq} className='w-100 ' />
-             </div>
+    <div className="container">
+      <div className="row my-5">
+        <div className={` border-sherd box-sherd  bg-sherd pb-4  `}  >
 
-             </div>
+          <div className="col-md-12">
+            <div className={`viewreq    d-flex justify-content-center `}>
+              <div className="myImg w-25  ">
+                <img src={imgReq} alt="" className='w-100 ' />
+              </div>
 
-<div className="col-md-12 my-5">
+            </div>
 
-  <div className="items ">
-  <ul className={`list-unstyled d-flex  justify-content-around  ${style.mydiv}`}>  
-<li>Blood Group</li>
-           <li>Name</li>
-           <li>Gender</li>
-           <li>Contact No</li>
-           <li>Mobile No</li>
-           <li>Email</li>
-<li>Till Required Date</li>
+            <div className="col-md-12 my-5">
 
-</ul>
+              <div className="items ">
+                <ul className={`list-unstyled d-flex  justify-content-around  ${style.mydiv}`}>
+                  <li className = {"text-danger"}>Blood Group</li>
+                  <li>Name</li>
+                  <li>Gender</li>
+                  <li>Contact No</li>
+                  <li>Mobile No</li>
+                  <li>Email</li>
+                  <li>Till Required Date</li>
+
+                </ul>
+              </div>
+
+
+
+
+
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
+
     </div>
 
-           
-
-
-
-  </div>
-
-          
-
-           </div>
-           </div>
-         </div>
-
-       </div>
-   
   )
 }
