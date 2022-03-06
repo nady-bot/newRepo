b893842557c8e55feeb0563edc4e0b5af1dff5be
Nady <74647837+nady-bot@users.noreply.github.com>
2022-03-06 18:38:57 +0200

    Update Login.jsx

--- blood_bank/src/Component/Login/Login.jsx
+++ blood_bank/src/Component/Login/Login.jsx
@@ -2,50 +2,51 @@ import React from 'react';
 import logInTwo from '../../images/login2.png'
 import logInOne from '../../images/login1.png'
 
+import style from "./Login.module.css"
+
 export default function Login() {
   return (
 
-    <div className="container">
-      <div className="row  d-flex justify-content-center">
-        <div className="col-md-4">
-          <div className="alldivs">
-            <div className="img w-100">
-              <img src={logInTwo} className='w-100' />
-              </div>
-
-              <div className="Img-form d-flex justify-content-center">
-                <div className="img2">
-                  <img src={logInOne} className='w-100'/>
-                  </div>
-
-                  <form>
-
-                    <div className="inputs">
-                    <label className='' htmlFor='email'>Email:</label>
-                    <input type='email' name='email'  className=''/>
-                      </div>
-
-                 <div className="inputs">
-                    <label className='' htmlFor='password'>Password:</label>
-                    <input type='password' name='password' className='' />
-                     </div>
-                   
-
-                    <button className='btn btn-danger mt-2'>login</button>
-
-                  </form>
+    <div className={`mx-auto my-5 ${style.login} bg-sherd border-sherd box-sherd py-3 text-center`} >
 
 
 
+      <div className="img w-100   ">
+        <img src={logInTwo} className=' w-50' alt="" />
+      </div>
 
+      <div className={`${style.login_form} py-4`}>
 
-                </div>
-            
-            
-            
-            </div>
-          </div>
+        <div className={` ${style.div_img2}   `} >
+          <img src={logInOne} className=' w-100' alt="" />
         </div>
+
+        <div className="flex-grow-1 d-flex justify-content-center    ">
+          <form>
+            <table>
+
+
+              <tr >
+                <td  ><label className='' htmlFor='email'>Email:</label> </td>
+                <td><input type='email' name='email' className='' /> </td>
+              </tr>
+
+              <tr>
+                <td> <label className='' htmlFor='password'>Password:</label></td>
+                <td> <input type='password' name='password' className='' /></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td><button className='btn  w-100 btn-danger mt-2'>login</button></td>
+              </tr>
+            </table>
+
+          </form>
         </div>
+
+        
+      </div>
+      <div> <p> Not A DONOR.?	Click here to REGISTER.  </p></div>
+    </div>
   )
 }
